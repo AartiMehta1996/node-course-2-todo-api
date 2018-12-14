feb9676b2f6727df11b56253dfbb6e4f705619f4
lcom147-two <aartimehta19963@gmail.com>
2018-12-14 15:43:33 +0530

    'msg'

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -6,8 +6,17 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 //const {User} = require('./../models/user');
+
+const todos = [{
+  text:'First test todo'
+},{
+  text:'Second test todo'
+}];
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=>done());
 });
 describe('POST /todos',() => {
   it('should create a new todo', (done) => {
@@ -25,7 +34,7 @@ describe('POST /todos',() => {
           return done(err);
       }
 
-      Todo.find().then((todos)=>
+      Todo.find({text}).then((todos)=>
       {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
